67894c14c47060133fc350455e6ff125cd901c2c
Michael0423 <ms800423@gmail.com>
2022-06-09 16:32:13 +0800

    Add 指定產圓功能

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function changeSVGSize() {
+    let svg = document.getElementById('svg'),
+        width = document.getElementById('width').value,
+        height = document.getElementById('height').value;
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+}
+
 function showParameters() {
     let type = document.getElementById('type').value;
     let circleConfig = document.getElementById('circle_config');
@@ -16,44 +24,39 @@ function showParameters() {
 }
 
 function create() {
-    let canvasWidth = document.getElementById('width').value,
-        canvasHeight = document.getElementById('height').value;
-    if(!canvasWidth || !canvasHeight) {
-        alert('請設定畫布大小');
-        return;
-    }
-
     let type = document.getElementById('type').value;
     if(!type) {
         alert('請先選擇圖像類型');
         return;
     }
-    console.log(type);
+    // console.log(type);
 
     let shape;
     switch(type){
         case 'circle':
-            shape = createEllipse();
+            let x = document.getElementById('cx').value, 
+                y = document.getElementById('cy').value, 
+                r = document.getElementById('r').value;
+                color= document.getElementById('color').value;
+            shape = createEllipse({ x, y }, r, color);
             break;
         case 'polygon':
             shape = createPolygon();
             break;
     }
 
-    let canvas = document.getElementById('canvas');
-    canvas.removeChild(canvas.firstChild);
-
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('width', canvasWidth);
-    svg.setAttribute('height', canvasHeight);
-    svg.style.border = '1px solid';
-    
     svg.appendChild(shape);
-    canvas.appendChild(svg);
 }
 
-function createEllipse() {
+function createEllipse(center, radius, color) {
+    let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
+    circle.setAttribute('cx', center.x);
+    circle.setAttribute('cy', center.y);
+    circle.setAttribute('r', radius);
+    circle.setAttribute('fill', color);
+
+    return circle;
 }
 
 function createPolygon() {
